f3a555a810449a49d386488b472e1ab8ca7f2c64
amirdarx <amirdarx@gmail.com>
2020-08-14 00:58:55 +0430

    last one

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Banji',
+    username: 'Banjiii',
+    email: 'banji@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Kasra mosavi',
+    username: 'Kasra',
+    email: 'Kasra@gmial.com'
+  },
+  {
+    id: 11,
+    name: 'Amir dark',
+    username: 'amirdarx',
+    email: 'Amirdarx@gmail.com'
+  },
+  {
+    id: 12,
+    name: 'taha',
+    username: 'Tahax',
+    email: 'Tahax@gmail.com'
+  }
+];
